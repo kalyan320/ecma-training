f36d39d6faf0a2824fd9b782331437377e7dd08d
kalyan320 <34162493+kalyan320@users.noreply.github.com>
2024-12-14 08:42:17 +0530

    Add files via upload

---
+++ ecma_assignment9/app.js
@@ -0,0 +1,5 @@
+import {pi, add, subtract} from "./math.js";
+
+console.log(pi);
+console.log(add(2,5));
+console.log(subtract(65,5));
---
+++ ecma_assignment9/math.js
@@ -0,0 +1,9 @@
+export const pi = 3.14;
+
+export function add(x,y){
+    return x+y;
+}
+
+export function subtract(x,y){
+    return x-y;
+}
